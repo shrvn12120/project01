09410f0a5066efcfdf9d50b6f0287ca90d72c348
Shifaz Ahmed <Paddey@paddeys-2.local>
2021-02-13 19:38:57 +0500

    loading image added

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 //preloader
-$(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(350).css({'overflow':'visible'});
-})
+// $(window).on('load', function() { // makes sure the whole site is loaded 
+//   $('#status').fadeOut(); // will first fade out the loading animation 
+//   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+//   $('body').delay(350).css({'overflow':'visible'});
+// })
 
 //for navbar
 window.onscroll = function() {scrollFunction()};
